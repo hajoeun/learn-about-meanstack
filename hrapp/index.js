81e0136273fd7f9bd24a2e05477883f359169487
Joeunha <imjoeunha@gmail.com>
2016-02-18 11:04:32 +0900

    hrapp, express 적용 완료 (13강 완료)

--- hrapp/index.js
+++ hrapp/index.js
@@ -1,57 +1,52 @@
-var http = require('http');
-var colors = require('colors');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
 require('./lib/connection');
-var employeeService = require('./lib/employees');
-var responder = require('./lib/responseGenerator');
-var staticFile = responder.staticFile('/public');
+var employees = require('./routes/employees');
+var teams = require('./routes/teams');
 
+var app = express();
 
-http.createServer(function (req, res) {
- var _url;
+// app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
- res.method = req.method.toUpperCase();
- console.log(req.method + ' ' + req.url);
+// 애플리케이션 라우트
+app.use(employees);
+app.use(teams);
 
- if (req.method !== 'GET') {
-  res.writeHead(501, {
-   'Content-Type': 'text/plain'
+// 404를 잡아 오류 처리기로 전달
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+
+  err.status = 404;
+  next(err);
+});
+
+// 오류 처리기
+
+// 스택 추적을 출력하는 개발자용 오류 처리기
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.send({
+      message: err.message,
+      error: err
+    });
   });
-  return res.end(req.method + ' is not implemented by this server.');
- }
+}
 
- if (_url = /^\/employees$/i.exec(req.url)) {
-  //직원 목록 반환
-  employeeService.getEmployees(function (error, data) {
-   if (error) {
-    //500 오류 전송
-    return responder.send500(error, res);
-   }
-   //200 상태 코드와 함께 데이터 전송
-   return responder.sendJson(data, res);
-  });
- } else if (_url = /^\/employees\/(\d+)$/i.exec(req.url)) {
-  //라우트에 포함된 id로 직원 검색
-  employeeService.getEmployee(_url[1], function (error, data) {
-   if (error) {
-    //500 오류 전송
-    return responder.send500(error, res);
-   }
+// 실제 서비스용 오류 처리기
+// 사용자에게 스택 추적을 유출하지 않는다.
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+});
 
-   if (!data) {
-    //404 오류 전송
-    return responder.send404(res);
-   }
-
-   //200 상태 코드와 함께 데이터 전송
-   return responder.sendJson(data, res);
-  })
- } else {
-  //파일이 존재하면 정적 파일 전송 시도
-  staticFile(req.url, res);
-  // res.writeHead(200);
-  // res.end('static file maybe');
-  //그렇지 않으면 404 오류 전송
- }
-}).listen(1337, '127.0.0.1');
-
-console.log('Server running at http://127.0.0.1:1337');
+module.exports = app;
